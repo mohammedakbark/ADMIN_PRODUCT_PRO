85ae2548650ce13e3a767221960a1c78648d1fe6
mohammedakbark <akbarmohd00123@gmail.com>
2024-02-16 13:51:43 +0530

    firebase and SHA updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,16 @@ import 'package:adminpanel_hardwarepro/View/pages/products/product.dart';
 import 'package:adminpanel_hardwarepro/View/pages/wareenty/claim_warrenty.dart';
 import 'package:adminpanel_hardwarepro/View/pages/wareenty/manage_warrenty.dart';
 import 'package:adminpanel_hardwarepro/View/profile.dart';
+import 'package:adminpanel_hardwarepro/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
